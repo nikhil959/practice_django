af921d4739f98d52d7f871118529d14388568820
unknown <nikhil1997times@gmail.com>
2022-07-06 21:13:40 +0530

    Fixes

--- first_django/first_blog/views.py
+++ first_django/first_blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'home.html', {})
+    posts = Post.objects.filter()
+    return render(request, 'home.html', {'posts': posts})
 
